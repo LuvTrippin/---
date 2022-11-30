44f10ec12fa0ce0622156f370fdcb9ca3b3b8dd0
LuvTrippin <82363203+LuvTrippin@users.noreply.github.com>
2022-11-30 16:22:02 +0500

    Add files via upload

--- laba7.py
+++ laba7.py
@@ -1,6 +1,3 @@
-import math
-
-
 def calc_omega(x: float, X: list) -> float:
     omega = 1
     for i in X:
@@ -16,24 +13,28 @@ def calc_d_omega(x: float, X: list) -> float:
     return d_omega
 
 
-def calc_c(new_x: list, xi: list) -> float:
-    c = 0
-    for k in range(N):
-        c = sum([calc_omega(i, xi)/(i - xi[k])/calc_d_omega(xi[k], xi) for i in new_x])
+def calc_c(new_x: list, xi: list) -> list:
+    c = list()
+    om = [calc_omega(i, xi) for i in new_x]
+    for k in range(N + 1):
+        d_om = calc_d_omega(xi[k], xi)
+        tmp = [i - xi[k] for i in new_x]
+        c.append(sum([i/(t*d_om)*h for i in om for t in tmp]))
     return c
 
 
 def calc_integral(new_x: list, xi: list) -> float:
-    I = sum([calc_c(new_x, xi) * f[i] for i in range(N)])
+    c = calc_c(new_x, xi)
+    I = sum([c[i] * f[i] for i in range(N + 1)])
+    return I
 
 
-f = []
-seg = [0.2, 1]
-a, b = 0.2, 1
+seg = [1, 2]
+a, b = 1, 2
 step = 0.2
 N = 5
 h = (b - a)/N
 x = [a+i*h for i in range(N + 1)]
 x_new = [i - 0.5*h for i in x]
-D_omg = [i - j for i in x for j in x if j != i]
-C = sum([])
+f = [i/(1 + i**2)**2 for i in x]
+calc_integral(x_new, x)
